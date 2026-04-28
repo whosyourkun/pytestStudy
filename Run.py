72db70a0df406309645619abcfdb979c71731127
whosyourkun <zdk4174@163.com>
2026-04-28 23:38:15 +0800

    Add generation of allure reports

--- Run.py
+++ Run.py
@@ -1,2 +1,17 @@
+import time
+
 import pytest,os
 
+if __name__ == '__main__':
+    pytest.main(['./tests/test_allure.py',
+                 '-vs',
+                 '--alluredir=temps',
+                 '--clean-alluredir'])
+    time.sleep(1)
+    if os.listdir("./temps"):
+        print('生成测试报告')
+        os.system(f"allure generate -o ./reports -c ./temps")
+    else:
+        print('未生成测试报告')
+    # if os.listdir('./temps'):
+    #     print(os.listdir('./temps'))
--- tests/test_allure.py
+++ tests/test_allure.py
@@ -2,5 +2,6 @@ import datetime
 import allure
 import pytest
 
+@pytest.mark.pay
 def test_1():
     print(datetime.datetime.now())
